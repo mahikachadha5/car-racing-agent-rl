0cb9bb4a7e98f9e87192a164f1f6ea22847ea13b
mahikachadha5 <chadhama5@gmail.com>
2026-02-10 17:03:39 -0500

    implement replay buffer

---
+++ replay_buffer.py
@@ -0,0 +1,15 @@
+import random
+from collections import deque
+
+class ReplayBuffer:
+    def __init__(self, capacity=100000):
+        self.buffer = deque(maxlen=capacity)
+            
+    def append(self, transition):
+        self.buffer.append(transition)
+        
+    def sample(self, sample_size):
+        return random.sample(self.buffer, sample_size)
+    
+    def __len__(self):
+        return len(self.buffer)
