11c727718bd830d335cef31f8deda2042c705e84
mahikachadha5 <chadhama5@gmail.com>
2026-02-18 11:32:49 -0500

    fix img.py bug

--- img.py
+++ img.py
@@ -6,6 +6,7 @@ def preprocess(img):
     # resizing the 96x96 img to 84x84
     img = img[:84, 6:90] # only take the first 84 rows (cut off black bar at bottom) and take the columns at the center of the track
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = img / 255.0
     return img
     
 class ImageEnv(gym.Wrapper):
